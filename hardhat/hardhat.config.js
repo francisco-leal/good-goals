7dc9bc0f5c3f173cfbcb7f7e7bcbde66a7705264
phil3k3 <philip.limbeck@gmail.com>
2023-11-17 23:32:11 +0300

    First complete version of contract

--- hardhat/hardhat.config.js
+++ hardhat/hardhat.config.js
@@ -10,9 +10,26 @@ module.exports = {
       url: "https://rpc.ankr.com/gnosis",
       accounts: [`0x${PRIVATE_KEY}`]
     },
-    celo: {
+    alfajores: {
       url: "https://alfajores-forno.celo-testnet.org",
-      accounts: [`0x${PRIVATE_KEY}`]
+      accounts: [`0x${PRIVATE_KEY}`],
+      chainId: 44787,
     }
-  }
+  },
+  etherscan: {
+    apiKey: {
+      alfajores: "9FYBMCJ8MJEF9Z6Y64MY9CBNF95KRE3VMJ",
+      celo: "9FYBMCJ8MJEF9Z6Y64MY9CBNF95KRE3VMJ"
+    },
+    customChains: [
+      {
+        network: "alfajores",
+        chainId: 44787,
+        urls: {
+          apiURL: "https://api-alfajores.celoscan.io/api",
+          browserURL: "https://alfajores.celoscan.io"
+        }
+      }
+    ]
+  },
 };
