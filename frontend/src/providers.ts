6e0504e2b02ed4602e97611c8e518c02cde0788e
johra444 <ylian.veron@gmail.com>
2023-11-17 13:36:22 +0300

    ylian

--- frontend/src/providers.ts
+++ frontend/src/providers.ts
@@ -9,6 +9,7 @@ if (!WALLETCONNECT_ID) {
   throw new Error('Missing NEXT_PUBLIC_WALLETCONNECT_ID')
 }
 
+
 export const chains = [mainnet, goerli]
 
 const { publicClient, webSocketPublicClient } = configureChains(chains, [
