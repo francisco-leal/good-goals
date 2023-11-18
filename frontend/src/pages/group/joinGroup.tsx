35c37b828e8f325d13bee4e9eb8cbc9d008fb34a
francisco-leal <francisco@talentprotocol.com>
2023-11-18 10:32:29 +0300

    UI tweaks

--- frontend/src/pages/group/joinGroup.tsx
+++ frontend/src/pages/group/joinGroup.tsx
@@ -10,8 +10,8 @@ export default function CreateGroup() {
               width="24"
               height="24"
               xmlns="http://www.w3.org/2000/svg"
-              fill-rule="evenodd"
-              clip-rule="evenodd"
+              fillRule="evenodd"
+              clipRule="evenodd"
               className="absolute left-8 mt-2"
             >
               <path d="M2.117 12l7.527 6.235-.644.765-9-7.521 9-7.479.645.764-7.529 6.236h21.884v1h-21.883z" />
