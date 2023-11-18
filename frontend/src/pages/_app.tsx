9646c542e44d60b28262333a2d9a5694f3c7720a
francisco-leal <francisco@talentprotocol.com>
2023-11-18 09:41:19 +0300

    change _app to use Thorin

--- frontend/src/pages/_app.tsx
+++ frontend/src/pages/_app.tsx
@@ -1,9 +1,10 @@
 import '@rainbow-me/rainbowkit/styles.css'
 import { RainbowKitProvider } from '@rainbow-me/rainbowkit'
-
-import {ThemeProvider} from 'next-themes'
+import { ThemeProvider } from 'styled-components'
+import { ThorinGlobalStyles, lightTheme } from '@ensdomains/thorin'
 import { WagmiConfig } from 'wagmi'
 import type { AppProps } from 'next/app'
+
 import '@/styles/globals.css'
 import { chains, wagmiConfig } from '@/providers'
 import { DefaultSeo } from 'next-seo'
