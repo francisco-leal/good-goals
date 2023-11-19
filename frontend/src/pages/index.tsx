cc96e4351461b7db5e81e5d3b7235f615281d8cf
francisco-leal <francisco@talentprotocol.com>
2023-11-19 09:42:05 +0300

    Add video to main screen

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -1,16 +1,28 @@
 import { NextSeo } from "next-seo";
 import { useAccount } from "wagmi";
 import Link from "next/link";
-import { Button } from "@ensdomains/thorin";
+import { Button, Dialog, Typography } from "@ensdomains/thorin";
 import { useWeb3Modal } from "@web3modal/wagmi/react";
+import { useEffect, useState } from "react";
 
- 
 export default function Home() {
   const { isConnected } = useAccount();
   const { open } = useWeb3Modal();
+  const [openModal, setOpenModal] = useState(false);
+  useEffect(() => { setOpenModal(true) }, []);
   return (
     <div className="w-full h-[100vh] flex justify-center flex-col items-center px-4">
       <NextSeo title={"Grow or Gamble"} />
+      <Dialog
+        alert="info"
+        open={openModal}
+        subtitle="We have a quick video to show what this app is all about!"
+        title="Watch the demo"
+        onDismiss={() => setOpenModal(false)}
+        variant="closable"
+      >
+        <Button onClick={() => window.open("https://tella.video/gg-take-2-4ga7", "_blank")}>Watch video!</Button>
+      </Dialog>
       {isConnected ? (
         <div className="w-full h-[100vh] flex flex-col items-center justify-around">
           <h1 className="mb-4 text-5xl text-center font-bold">Gamble</h1>
